9f24405e88ad027def8ea5d898b9893b51fab31a
venegassss <cristopheribujes@gmail.com>
2026-03-30 13:09:53 +0000

    Upgrades installed packages to newer versions.

--- a.sh
+++ a.sh
@@ -1,3 +1,7 @@
 // Updates the package lists from repositories.
     sudo apt update
 //Shows repository URLs, fetched data, and package list status.
+
+//Upgrades installed packages to newer versions.
+    sudo apt upgrade
+//Shows packages to be upgraded, download size, and installation progress.
