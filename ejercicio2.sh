74f4a6b792529da70761a8333edca213d4c8916c
venegassss <cristopheribujes@gmail.com>
2026-04-15 13:37:20 +0000

    ejercicio2.sh

---
+++ ejercicio2.sh
@@ -0,0 +1,10 @@
+touch prueba.txt
+#creates an empty file
+chmod 600 prueba.txt
+#sets permissions: owner read/write only
+ls -l prueba.txt
+#shows detailed file permissions and info
+chmod 755 prueba.txt
+#sets permissions: owner full, others read/execute
+ls -l prueba.txt
+#shows updated permissions and file info
