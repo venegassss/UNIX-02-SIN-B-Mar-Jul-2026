46ec2d58c992809879fe1723e3795555eb15528d
venegassss <cristopheribujes@gmail.com>
2026-04-15 13:04:37 +0000

    ejercicio_1 command 2

--- ejercicio_1.sh
+++ ejercicio_1.sh
@@ -1,2 +1,5 @@
 echo '#!/bin/sh' > hola.sh
 #Create hola.sh with the shebang (define that it uses /bin/sh)
+
+echo 'echo "hola desde mi primer script"' >> hola.sh
+#Add the command that will print the message
