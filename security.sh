ae6426bb3c095717a38ab5b36c0195acd254de21
venegassss <cristopheribujes@gmail.com>
2026-05-06 12:41:29 +0000

    GIDs

--- security.sh
+++ security.sh
@@ -11,3 +11,12 @@ grep -E "desarrolladores\|operaciones\|servicios_web" /etc/group
 
 #see main options
 groupadd --help
+
+#View the range of GIDs in the system
+grep "GID_MIN\|GID_MAX\|SYS_GID" /etc/login.defs
+
+#ubuntu
+SYS_MIN = 100
+SYS_MAX = 999
+GID_MIN = 1000
+GID_MAX = 60000
