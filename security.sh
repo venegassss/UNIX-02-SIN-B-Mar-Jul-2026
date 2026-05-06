74b829888e74db056d3dc0388e643e97334e48bb
venegassss <cristopheribujes@gmail.com>
2026-05-06 12:37:28 +0000

    verify

--- security.sh
+++ security.sh
@@ -3,3 +3,11 @@ sudo groupadd desarrolladores
 sudo groupadd -g 2000 operaciones #SPECIFIC GID
 # system group
 sudo groupadd --system servicios_web
+
+#verify that they were created
+grep "desarrolladores\|operaciones\|servicios_web" /etc/group
+
+grep -E "desarrolladores\|operaciones\|servicios_web" /etc/group
+
+#see main options
+groupadd --help
