245e5511578130374f99d305b862c700fee5c75a
Cristopher Venegas <crvenegasib@uide.edu.ec>
2026-06-01 13:40:50 +0000

    variables blackhatbash3.sh

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -2,9 +2,21 @@
 set -x
 
 book="black hat bash"
+
 echo "This book's name is ${book}"
 #book="black hat bash"
     #Creates a variable called book and stores the text "black hat bash" inside it.
+
 #echo "This book's name is ${book}"
     #Prints a sentence and inserts the value of the variable using ${book}.
+
+echo "This book's name is $book"
+#This prints a sentence and expands the variable book directly using $book.
+
+root_directory=$(ls -ld /)
+#stores detailed info about the root directory / in a variable.
+
+echo "${root_directory}"
+#prints that stored information
+
 set +x
