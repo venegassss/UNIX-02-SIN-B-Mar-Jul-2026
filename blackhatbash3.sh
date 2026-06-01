1bf8b24c35f9f811f0aa4c05503d47f7b244a357
Cristopher Venegas <crvenegasib@uide.edu.ec>
2026-06-01 13:35:27 +0000

    blackhatbash3.sh

---
+++ blackhatbash3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+book="black hat bash"
+echo "This book's name is ${book}"
+#book="black hat bash"
+    #Creates a variable called book and stores the text "black hat bash" inside it.
+#echo "This book's name is ${book}"
+    #Prints a sentence and inserts the value of the variable using ${book}.
+set +x
