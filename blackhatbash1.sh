0468d9c8246a943a90131896b4e0e37aa33a57e9
Cristopher Venegas <crvenegasib@uide.edu.ec>
2026-06-01 13:00:16 +0000

    Shebang_blackhatbash

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+#!/bin/bash -x
+#tells the system to run the script using Bash and enables debug mode 
+#(-x), which prints each command and its arguments as they are executed.
 bash --version
 env
 echo ${SHELL}
