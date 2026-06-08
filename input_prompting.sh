64c6fdc9169a9020afe285f2f390af6b3fd49013
Cristopher Venegas <crvenegasib@uide.edu.ec>
2026-06-08 13:12:23 +0000

    input_prompting.sh

---
+++ input_prompting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+echo "what is your first name?"
+read -r firstname
+
+echo "what is your last name?"
+read -r lastname
+echo "your first name is ${firstname} and your last name is ${lastname}"
