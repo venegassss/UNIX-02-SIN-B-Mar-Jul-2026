7edde1141164992a8ff8750d181631d771cd0606
Cristopher Venegas <crvenegasib@uide.edu.ec>
2026-06-08 12:57:04 +0000

    ping blackhatbash6.sh

--- blackhatbash6.sh
+++ blackhatbash6.sh
@@ -1,14 +1,19 @@
-#!/bin/bash 
+#!/bin/bash
+
 set -x
-ls -l / | grep "bin"
-SCRIPT_NAME="$(0)"
-TARGET="$(1)"
 
-echo "Running the script ${SCRIPT_NAME}..."
-echo "Pinging the target: ${TARGET}..."
-ping "${TARGET}"
+SCRIPT_NAME="$0"
+TARGET="$1"
 
+echo "Running the script $SCRIPT_NAME ..."
+echo "Pinging the target: $TARGET ..."
 
+ping -c 3 "$TARGET"
 
+echo "the arguments are: $0"
+echo "the total number of arguments is: $#"
+
+for args in "$0"; do
+    echo "${args}"
+done
 set +x
-
