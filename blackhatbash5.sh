4174e8ec90bab8a4bf2af74bff3ef83ab638b615
Cristopher Venegas <crvenegasib@uide.edu.ec>
2026-06-08 12:27:08 +0000

    blackhatbash5 EOF

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -14,4 +14,16 @@ cat archivo.txt
 echo 11 >> archivo.txt
 cat archivo.txt
 
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stderr.txt
+lzl 2> error.txt 
+cat error.txt
+cat < output.txt
+
+    cat << EOF 
+    Black Hat Bash 
+    by No Starch Press 
+EOF
 set +x
