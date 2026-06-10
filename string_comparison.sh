4e87324693150f8ecfcce3a86d8138823c300f5d
Cristopher Venegas <crvenegasib@uide.edu.ec>
2026-06-10 12:31:16 +0000

    string_comparison.sh

---
+++ string_comparison.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VARIABLE_ONE="nostarch"
+VARIABLE_TWO="nostarch"
+
+if [[ "${VARIABLE_ONE}" == "${VARIABLE_TWO}" ]]; then
+    echo "They are equal!"
+else
+    echo "They are not equal!"
+fi
--- test_if_file_exists.sh
+++ test_if_file_exists.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-FILENAME="flow_control_with_if.txt"
+FILENAME="flow_control_with_if_1.txt"
 
-if [ -f "${FILENAME}" ]; then
-    echo "${FILENAME} already exists."
-    exit 1
-else
+if [ ! -f "${FILENAME}" ]; then
     touch "${FILENAME}"
 fi
