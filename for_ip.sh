c9a3782b7ab65e19eea6e8b10eb9d2405a75bb6f
Cristopher Venegas <crvenegasib@uide.edu.ec>
2026-06-15 12:39:27 +0000

    for_ip.sh2

---
+++ for_ip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for ip_address in "$@"; do
+    echo "Taking some action on IP address ${ip_address}"
+done
+
+#./for_ip.sh 10.0.0.1 10.0.0.2 192.168.1.1 192.168.1.2
+
+
+for file in $(ls .); do
+    echo "File: ${file}"
+done
