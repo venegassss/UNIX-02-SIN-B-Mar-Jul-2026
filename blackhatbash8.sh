3a0a12ccd0e53705b042caa7ec86fd13c5f8ce81
Cristopher Venegas <crvenegasib@uide.edu.ec>
2026-06-17 12:57:03 +0000

    sed blackhatbash8.sh

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -11,3 +11,14 @@ awk -F',' '{print $1}' example_csv.txt
 head log.txt 10 #lines
 
 grep "42.236.10.117" log.txt | awk '{print $7}'
+
+sed 's/Mozilla/Godzilla/g' log.txt
+
+grep "Godzilla" log.txt #I use grep, but the sed command only 
+#modifies the output and doesn't change the file, therefore it doesn't display anything.
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+grep "Godzilla" newlog.txt
+#Now it works
+sed 's/ //g' log.txt
+sed 's/ //g' log.txt > newlog1.txt
+cat newlog1.txt
