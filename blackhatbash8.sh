e5045d2f62e45a28f9719d8877f75c18a178e67f
Cristopher Venegas <crvenegasib@uide.edu.ec>
2026-06-17 12:44:37 +0000

    grep and awk

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -3,7 +3,11 @@ awk '{print $1,$2,$3}' log.txt
 awk '{print $1}' log.txt
 awk '{print $2}' log.txt
 awk '{print $3}' log.txt
+
 awk '{print $1,$NF}' log.txt
 awk '{print $NF}' log.txt
+
 awk -F',' '{print $1}' example_csv.txt
 head log.txt 10 #lines
+
+grep "42.236.10.117" log.txt | awk '{print $7}'
